90be44b0f1098676bb7f5c31b426bdda5c0d4a72
Matteu99 <mateusz.wawrzyniak.1@gmail.com>
2024-10-27 21:43:25 +0100

    initial comit

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,28 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    private:
+    double x;
+    double y;
+
+public:
+
+    Wektor2D() : x(0.0), y(0.0) {}
+
+    Wektor2D(double x_val, double y_val) : x(x_val), y(y_val) {}
+
+    void setX(double x_val) { x = x_val; }
+
+    void setY(double y_val) { y = y_val; }
+
+    double getX() const { return x; }
+
+    double getY() const { return y; }
+
+    Wektor2D operator+(const Wektor2D& other) const {
+        return Wektor2D(x + other.x, y + other.y);
+    }
+
+    double operator*(const Wektor2D& other) const {
+        return x * other.x + y * other.y;
+    }
 };
